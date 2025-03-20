aa4b83d5d0e12f36315f11182539074fb53f0110
Henrique Ferreira da Silva <00001213449856sp@al.educacao.sp.gov.br>
2025-03-19 23:58:08 -0300

    atualização

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-app = Flask (__name__)
+app = Flask(__name__)
+CORS(app, origins='*')
 
-@app.route('/test', methods =['GET'])
-def test():
-    return jsonify({'message': 'Ola mundo'})
+usuarios = [
+    'Kaidy',
+    'Renna',
+    'Julia',
+    'Yasmin',
+    'Felipe',
+    'Malta',
+    'Henrique'
+]
 
-@app.route('/usuario/novo', methods =['POST'])
-def criar_novo_usuario():
+@app.route('/users',methods=['GET'])
+def pegar_usuarios():
+    return jsonify({'users':usuarios})
+
+@app.route('/user/<numero>', methods=['GET'])
+def pegar_usuario(numero):
+    parsednumero = int(numero)
+    return jsonify({'user': usuarios[parsednumero]})
+
+@app.route('/usernovo',methods=['POST'])
+def criar_usuario():
+    print('Ola')
     novo_usuario = request.json
     print(novo_usuario)
-    return jsonify({
+    return jsonify ({
         'user': novo_usuario,
-        'message': 'Usuario criado com sucesso!'
-        })
+        "message": "Usuario foi criado com sucesso!"})
     
-if __name__ =='__main__':
-    app.run(port=5000)
+if __name__ == '__main__':
+    app.run(port=3000)
+    
